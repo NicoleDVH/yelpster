d7f94e75b24c9f508706c82500090ecfda84c902
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-07-09 09:00:59 -0400

    added create function to restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,25 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number)
+  end
+
 end

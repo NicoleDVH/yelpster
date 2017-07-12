41f0e4490fb3fbf8f37639d2b16230fbbaf916e7
NicoleDVH <nicole12909@gmail.com>
2017-07-12 11:56:55 -0400

    Added links to index page

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	RSpec.describe RestaurantsController, type: :controller do
+		describe "POST #create" do
+    		context "with valid attributes" do
+      			it "create new restaurant" do
+			        post :create, restaurant: attributes_for(:restaurant)
+			        expect(restaurant.count).to eq(1)
+			    end
+		end
+	end
+
 end
